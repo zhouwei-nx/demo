41481e67579413f5aa3e21894878dd8cf4693c79
1300012858 <1300012858@qq.com>
2022-09-02 13:45:27 +0800

    init

---
+++ app/src/test/java/com/bjyx/demo/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package com.bjyx.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.bjyx.hello_lib.hello;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        hello.log();
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ hello-lib/src/main/java/com/bjyx/hello_lib/hello.java
@@ -0,0 +1,7 @@
+package com.bjyx.hello_lib;
+
+public class hello {
+    public static void log() {
+        System.out.println("init ok");
+    }
+}
